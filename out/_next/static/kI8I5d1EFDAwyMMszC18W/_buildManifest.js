a5444cf9473d00d41e9bb8574c53087866afac54
Paulo Silva <paulo.silva@impar.com.br>
2022-01-31 11:18:40 -0300

    Adicionar estaticos

---
+++ out/_next/static/kI8I5d1EFDAwyMMszC18W/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-97805c9252195231.js"],"/_error":["static/chunks/pages/_error-d742f979193aeae4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
